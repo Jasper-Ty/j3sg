73c395679349dd5fcdc70b97a7050f5b887f7610
Jasper Ty <jasperty2@gmail.com>
2023-08-07 16:54:24 -0400

    leave only main route and add environment variable

--- src/main.rs
+++ src/main.rs
@@ -10,27 +10,20 @@ use actix_web::{
 
 #[get("/")]
 async fn hello() -> impl Responder {
-    HttpResponse::Ok().body("Hello world!")
-}
-
-#[get("/echo")]
-async fn echo(req_body: String) -> impl Responder {
-    HttpResponse::Ok().body(req_body)
-}
-
-async fn manual_hello() -> impl Responder {
-    HttpResponse::Ok().body("Hey there!")
+    HttpResponse::Ok().body("jasper")
 }
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
+    let host = std::env::var("JTY_WEBSITE_HOST")
+        .map(|host| (host, 80))
+        .unwrap_or(("127.0.0.1".to_string(), 8080));
+
     HttpServer::new(|| {
         App::new()
             .service(hello)
-            .service(echo)
-            .route("/hey", web::get().to(manual_hello))
     })
-    .bind(("127.0.0.1", 8080))?
+    .bind(host)?
     .run()
     .await
 }
