b6605ed29615a5e335c6f600200b41645e04fa24
Jasper Ty <jasperty2@gmail.com>
2023-08-09 09:41:13 -0400

    add blog and update hook

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,32 @@
 use actix_files as fs;
-use actix_web::{ App, HttpServer };
+use actix_web::{ 
+    post,
+    App, 
+    HttpServer,
+    HttpResponse,
+    Responder,
+};
 use actix_web::dev::{ ServiceRequest, ServiceResponse, fn_service };
+use std::process::Command;
+
+#[post("/update")]
+async fn update() -> impl Responder {
+    let update_script = std::env::var("JTY_WEBSITE_UPDATE_SCRIPT")
+        .unwrap_or(String::from("ls"));
+    Command::new(update_script)
+        .output()
+        .ok() 
+        .and_then(|o| String::from_utf8(o.stdout).ok())
+        .map(|o| HttpResponse::Ok().body(format!("Success. Output: {}", o)))
+        .unwrap_or(HttpResponse::InternalServerError().body("Unsuccessful"))
+}
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    let host = std::env::var("JTY_WEBSITE_HOST")
+    let bind_addr = std::env::var("JTY_WEBSITE_BIND_ADDR")
         .map(|s| (s, 80))
         .unwrap_or(("127.0.0.1".to_string(), 3000));
-    println!("Listening on {}:{}", host.0, host.1);
+    println!("Listening on {}:{}", bind_addr.0, bind_addr.1);
 
     let static_path = std::env::var("JTY_WEBSITE_STATIC_PATH")
         .unwrap_or(String::from("./static"));
@@ -18,6 +37,7 @@ async fn main() -> std::io::Result<()> {
 
     HttpServer::new(move || {
         App::new()
+            .service(update)
             .service(
                 fs::Files::new("/static", static_path.clone())
                     .show_files_listing()
@@ -34,7 +54,7 @@ async fn main() -> std::io::Result<()> {
                     }))
             )
     })
-    .bind(host)?
+    .bind(bind_addr)?
     .run()
     .await
 }
