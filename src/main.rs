e3ed8f6bf084ad338b320a737fbb6da7f3c7aaf7
Jasper Ty <jasperty2@gmail.com>
2023-08-18 17:31:56 -0400

    try tera, remove pages_src, add mononoki to stylesheet, remove self-signed certs

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,11 @@
-use std::process::Command;
-
 use actix_files as fs;
 use actix_web::{ 
-    post,
     App, 
     HttpServer,
-    HttpResponse,
-    Responder,
 };
 use actix_web::dev::{ ServiceRequest, ServiceResponse, fn_service };
 use openssl::ssl::{ SslAcceptor, SslFiletype, SslMethod };
-
+use tera::{ Tera, Context };
 
 /// Gets env variable or a default value if it doesn't exist
 /// 
@@ -23,24 +18,32 @@ macro_rules! env_or {
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    let bind_addr = env_or!("JTY_WEBSITE_BIND_ADDR", "127.0.0.1:3000");
+    let bind_addr = env_or!("JTY_WEBSITE_BIND_ADDR", "127.0.0.1:5000");
     let static_path = env_or!("JTY_WEBSITE_STATIC_PATH", "./static");
     let pages_path = env_or!("JTY_WEBSITE_PAGES_PATH", "./pages");
-    let tls_key = env_or!("JTY_WEBSITE_TLS_KEY", "./dev/dev_key.pem");
-    let tls_cert = env_or!("JTY_WEBSITE_TLS_CERT", "./dev/dev_cert.pem");
+
+    let tera = match Tera::new("templates/*.html") {
+        Ok(t) => t,
+        Err(e) => {
+            println!("Parsing error(s): {}", e);
+            ::std::process::exit(1);
+        }
+    };
+
+    let mut context = Context::new();
+    context.insert("page_title", "Jasper Ty");
+    context.insert("header_content", "Header");
+    context.insert("main_content", "Main");
+    let index = tera.render("index.html", &context).unwrap();
+    println!("{}", index);
+
 
     println!("Listening on {}", bind_addr);
     println!("Static directory at {}", static_path);
     println!("Pages directory at {}", pages_path);
-    println!("Using TLS key/cert {}/{}", tls_key, tls_cert);
 
-    let mut builder = SslAcceptor::mozilla_intermediate(SslMethod::tls()).unwrap();
-    builder
-        .set_private_key_file(tls_key, SslFiletype::PEM)
-        .unwrap();
-    builder.set_certificate_chain_file(tls_cert).unwrap();
 
-    HttpServer::new(move || {
+    let http_server = HttpServer::new(move || {
         App::new()
             .service(
                 fs::Files::new("/static", &static_path[..])
@@ -57,8 +60,24 @@ async fn main() -> std::io::Result<()> {
                         Ok(ServiceResponse::new(req, res))
                     }))
             )
-    })
-    .bind_openssl(bind_addr, builder)?
-    .run()
-    .await
+    });
+
+
+    if let Ok(tls_key) = std::env::var("JTY_WEBSITE_TLS_KEY") {
+        let tls_cert = std::env::var("JTY_WEBSITE_TLS_CERT").unwrap();
+        println!("Using TLS key/cert {}/{}", tls_key, tls_cert);
+        let mut builder = SslAcceptor::mozilla_intermediate(SslMethod::tls()).unwrap();
+        builder
+            .set_private_key_file(tls_key, SslFiletype::PEM)
+            .unwrap();
+        builder.set_certificate_chain_file(tls_cert).unwrap();
+
+        http_server.bind_openssl(bind_addr, builder)?
+            .run()
+            .await
+    } else {
+        http_server.bind(bind_addr)?
+            .run()
+            .await
+    }
 }
